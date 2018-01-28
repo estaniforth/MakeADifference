e82161ee36f20f34ae82dba0dd73ec4f3befd60e
Madeline Helmstadter <madeline.helmstadter@gmail.com>
2018-01-28 01:35:56 -0500

    ProjectOverview - initial layout

---
+++ app/src/main/java/bitsplease/makeadifference/ProjectOverviewActivity.java
@@ -0,0 +1,55 @@
+package bitsplease.makeadifference;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class ProjectOverviewActivity extends AppCompatActivity {
+
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_project_overview);
+
+        listView = (ListView) findViewById(R.id.goal_list);
+
+        String[] goals = new String[] {
+            "Goal1","Goal2", "Goal3", "Goal4"
+        };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, goals);
+
+
+        // Assign adapter to ListView
+        listView.setAdapter(adapter);
+
+        // ListView Item Click Listener
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+                // ListView Clicked item index
+                int itemPosition     = position;
+
+                // ListView Clicked item value
+                String  itemValue    = (String) listView.getItemAtPosition(position);
+
+                // Show Alert
+                Toast.makeText(getApplicationContext(),
+                        "Position :"+itemPosition+"  ListItem : " +itemValue , Toast.LENGTH_LONG)
+                        .show();
+
+            }
+
+        });
+    }
+}
